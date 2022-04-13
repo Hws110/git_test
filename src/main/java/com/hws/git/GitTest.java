b523bc7ded522c9b97220b13a31138c77c93f2ba
Hws <Hws@QQ.com>
2022-04-14 01:18:42 +0800

    second commit

--- src/main/java/com/hws/git/GitTest.java
+++ src/main/java/com/hws/git/GitTest.java
@@ -3,5 +3,6 @@ package com.hws.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("这是一个测试!!");
+        System.out.println("这是一个测试second！！");
     }
 }
