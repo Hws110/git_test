dbe6fe5c3c6adaead8d8cf4f74f9341ab55df5ea
Hws <Hws@QQ.com>
2022-04-14 01:26:21 +0800

    hot-fix commit

--- src/main/java/com/hws/git/GitTest.java
+++ src/main/java/com/hws/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("这是一个测试!!");
         System.out.println("这是一个测试second！！");
+        System.out.println("hot-fix--------------");
     }
 }
